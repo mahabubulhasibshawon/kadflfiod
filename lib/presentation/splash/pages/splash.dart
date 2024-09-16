5d5ba9d6791522587e980175253ef3b150b6ef13
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-09-16 11:27:40 +0600

    Get Started Pgae

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import '../../intro/pages/get_started.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
-
   @override
   State<SplashPage> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,5 +22,11 @@ class _SplashPageState extends State<SplashPage> {
         child: SvgPicture.asset(AppVectors.logo),
       ),
     );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (BuildContext context) => const GetStartedPage()));
   }
 }
