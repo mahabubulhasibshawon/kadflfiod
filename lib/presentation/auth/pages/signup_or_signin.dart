b4fbb66bdebb26db76f1b98dcce101564f272a31
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-09-17 12:29:27 +0600

    signup_or_signin_page

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/helpers/is_dark_mode.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presentation/auth/pages/signin.dart';
+import 'package:spotify/presentation/auth/pages/signup.dart';
 
 class SignupOrSigninPage extends StatelessWidget {
   const SignupOrSigninPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: SvgPicture.asset(AppVectors.topPattern),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SvgPicture.asset(AppVectors.bottomPattern),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Image.asset(AppImages.authBG),
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(AppVectors.logo),
+                      const SizedBox(height: 55),
+                      const Text(
+                        'Enjoy Listening to Music',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 21,
+                      ),
+                      const Text(
+                        'Spotify is a proprietary Swedish audio streaming and media services provider',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: Colors.grey),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: BasicAppButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                      const SignupPage(),
+                                    ),
+                                  );
+                                },
+                                title: 'Register'),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                  onPressed: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            const SigninPage()));
+                                  },
+                                  child: Text(
+                                    'Sign in',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: context.isDarkMode
+                                            ? Colors.white
+                                            : Colors.black),
+                                  )))
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/common/widgets/button/basic_oval_btn.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/main.dart';
 import 'package:spotify/presentation/auth/pages/signup_or_signin.dart';
+import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 
 import '../../../core/configs/theme/app_colors.dart';
 
@@ -50,8 +53,11 @@ class ChooseModePage extends StatelessWidget {
                       Column(
                         children: [
                           GestureDetector(
-                            onTap: () {},
-                            child: const BasicOvalBtn(assetName: AppVectors.moon),
+                            onTap: () {
+                              context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                            },
+                            child:
+                                const BasicOvalBtn(assetName: AppVectors.moon),
                           ),
                           const SizedBox(
                             height: 15,
@@ -69,8 +75,11 @@ class ChooseModePage extends StatelessWidget {
                       Column(
                         children: [
                           GestureDetector(
-                            onTap: () {},
-                            child: const BasicOvalBtn(assetName: AppVectors.sun),
+                            onTap: () {
+                              context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                            },
+                            child:
+                                const BasicOvalBtn(assetName: AppVectors.sun),
                           ),
                           const SizedBox(
                             height: 15,
@@ -97,8 +106,7 @@ class ChooseModePage extends StatelessWidget {
                           ),
                         );
                       },
-                      title: 'Continue'
-                  ),
+                      title: 'Continue'),
                 ],
               ),
             )
